09570f5b4b0eea5e5cad70a0975b6cdb397d4352
Fabber12 <fabiano.tarulli@polito.it>
2025-07-18 19:02:05 +0200

    modulefiles correction

--- hpc/leonardo@cineca/lammps_mliap_29Aug2024_update3/build.sh
+++ hpc/leonardo@cineca/lammps_mliap_29Aug2024_update3/build.sh
@@ -12,19 +12,19 @@ echo "sourcing environments"
 
 module purge
 module load profile/chem-phys
-module load python/3.11.6--gcc--8.5.0
+module load python/3.11.7
 module load gcc/12.2.0
-module load gsl/2.7.1--gcc--12.2.0
-module load openmpi/4.1.6--gcc--12.2.0
-module load fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0
-module load openblas/0.3.24--gcc--12.2.0
+module load openblas/0.3.26--gcc--12.2.0
+module load gsl/2.7.1--gcc--12.2.0-spack0.22
+module load openmpi/4.1.6--gcc--12.2.0-cuda-12.2
+module load fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0-spack0.22
 module load cuda/12.1
-module load intel-oneapi-mkl/2023.2.0
-module load plumed/2.8.1--openmpi--4.1.6--gcc--12.2.0
+module load intel-oneapi-mkl/2024.0.0
+module load plumed/2.9.2--openmpi--4.1.6--gcc--12.2.0
 
 source $PUBLIC/programs/lammps/lammps_stable_29Aug2024_update3/build/.lammps-mliap/bin/activate
 
-cmake -C confing.make \
+cmake -C config.make \
   -D CMAKE_BUILD_TYPE=Release \
   -D CMAKE_INSTALL_PREFIX=$(pwd) \
   ../cmake
