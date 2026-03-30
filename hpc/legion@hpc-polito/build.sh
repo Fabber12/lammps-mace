4572289f5b6d0bdbd469252ffa3488fe3ca2f247
Fabiano Tarulli <fabiano.tarulli@polito.it>
2026-03-30 17:54:37 +0200

    Update build script for modules and paths

--- hpc/legion@hpc-polito/build.sh
+++ hpc/legion@hpc-polito/build.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 #SBATCH --job-name=mliap
 #SBATCH --mail-type=None
-#SBATCH --partition=gpu_a40
+#SBATCH --partition=gpu_h200
 #SBATCH --time=02:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=16
+#SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:1
 #SBATCH --output=slurm-%j.out
 #SBATCH --mem=32G
 
-cd $HOME/programs/lammps_mliap_22Jul2025
+cd $SCRATCH_FLASH/programs/lammps_mliap_11Feb2026/build
 mkdir -p build_mliap && cd build_mliap
-cp $HOME/programs/lammps_mliap_22Jul2025/cmake/presets/kokkos-cuda.cmake . 
+cp $SCRATCH_FLASH/programs/lammps_mliap_11Feb2026/cmake/presets/kokkos-cuda.cmake . 
 
 module purge
-module use $HOME/modules
+module use $SCRATCH_FLASH/modules
 module load miniforge/24.3.0-0
 module load cmake/4.1.1
 module load gcc/12.4.0
 module load openmpi/5.0.7_gcc12
 module load openblas/0.3.30
-module load cuda/12.1
+module load cuda/12.6 
 
 export CC=$(which gcc)
-export CXX=$(which g++)
 
-source $HOME/venv/.lammpsmace-mliap/bin/activate
+source $SCRATCH_FLASH/venv/.lammpsmace-mliap/bin/activate
 
 cmake -C kokkos-cuda.cmake \
   -D CMAKE_BUILD_TYPE=Release \
@@ -33,6 +32,7 @@ cmake -C kokkos-cuda.cmake \
   -D BUILD_MPI=ON \
   -D PKG_ML-IAP=ON \
   -D PKG_ML-SNAP=ON \
+  -D CMAKE_CUDA_ARCHITECTURES=90 \
   -D MLIAP_ENABLE_PYTHON=ON \
   -D PKG_PYTHON=ON \
   -D PKG_ML-MACE=ON \
@@ -46,7 +46,8 @@ cmake -C kokkos-cuda.cmake \
   -D PKG_MISC=ON \
   -D PKG_RIGID=ON \
   -D BUILD_SHARED_LIBS=ON \
+  ../cmake=ON \
   ../cmake
 
-make -j16
+make -j32
 make install-python
